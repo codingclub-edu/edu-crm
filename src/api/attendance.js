70babe09ed76786ebb3c14a3433f548b9f67b769
abdunozir <ibyvtnq@gmail.com>
2026-04-24 15:19:58 +0500

    fix: add missing lucide-react dep and API exports for build

--- src/api/attendance.js
+++ src/api/attendance.js
@@ -1,3 +1,5 @@
 import api from "./axios";
 
 export const getMyAttendance = () => api.get("/attendance/me/attendance");
+export const getGroupAttendance = (groupId) => api.get(`/attendance/group/${groupId}`);
+export const updateDayAttendance = (groupId, data) => api.post(`/attendance/group/${groupId}`, data);
--- src/api/groups.js
+++ src/api/groups.js
@@ -3,4 +3,5 @@ import api from './axios'
 export const getMyGroups = () => api.get('/groups/me')
 export const getAllGroups = () => api.get('/groups')
 export const getGroupById = (id) => api.get(`/groups/${id}`)
+export const getAllRooms = () => api.get('/rooms')
 export const updateGroup = (id, data) => api.put(`/groups/${id}`, data)
